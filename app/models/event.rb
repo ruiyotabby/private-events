101693add77075039915003d3176d4a6762c9b97
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-05 15:29:48 +0300

    Add validations and associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, uniqueness: true, presence: true
+  has_many :events, class_name: "Event", foreign_key: "creator_id"
 end
