80345bf19f50550d4a8ae00653cc78713893d100
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-05 15:26:22 +0300

    Add create and new actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,23 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to controller: :users, action: :show
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
+  end
 end
