2fce35566b302fbb5bb0346f2fc85ec77587d56e
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-06 03:54:35 +0300

    Add ticket to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_003859) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_06_004733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_003859) do
     t.datetime "updated_at", null: false
     t.bigint "creator_id"
     t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.bigint "attendee_id"
+    t.bigint "attended_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attended_event_id"], name: "index_tickets_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_tickets_on_attendee_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_003859) do
   end
 
   add_foreign_key "events", "users", column: "creator_id"
+  add_foreign_key "tickets", "events", column: "attended_event_id"
+  add_foreign_key "tickets", "users", column: "attendee_id"
 end
