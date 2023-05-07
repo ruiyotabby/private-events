58e35ed80ac60bf881d7188fb12d708df3b83b7c
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-07 21:44:09 +0300

    Add #create and #destroy actions

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,17 @@
 class TicketsController < ApplicationController
   def create
     event = Event.find(params[:event_id])
-    current_user.attended_events << event
-    redirect_to event
+    ticket = current_user.tickets.build(attended_event: event)
+
+    if ticket.save
+      redirect_to event, notice: 'Successfully attended the event.'
+    else
+      redirect_to event, alert: 'Failed to attend the event.'
+    end
   end
 
   def destroy
-    
+    event = Event.find(params[:event_id])
+    event.tickets.destroy.find_by(attendee: current_user)
   end
 end
