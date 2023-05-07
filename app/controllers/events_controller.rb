98cb9f72cc0127d5999432f374474f4df179def9
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-07 21:43:06 +0300

    Add #edit, #update and #destroy to the events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.includes(:creator).all
+  end
+
+  def show
+    @event = Event.find(params[:id])
   end
 
   def new
@@ -10,10 +14,31 @@ class EventsController < ApplicationController
   def create
     @event = current_user.events.build(event_params)
     if @event.save
-      redirect_to controller: :users, action: :show
+      redirect_to @event, notice: 'Event created successfully.'
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = current_user.events.build
+
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Successfully edited'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destory
+    @event = Event.find(params[:id])
+    @event.destory
+
+    redirect_to root_path, status: :see_other, notice: 'Event sucessfully deleted.'
   end
 
   private
