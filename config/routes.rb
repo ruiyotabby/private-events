9f4cd31c8f0243c7599f841d590762e8469c5830
Ruiyot Abby <ruiyotabby@gmail.com>
2023-05-07 22:01:27 +0300

    Add tickets routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :events
+  resources :events do
+    resources :tickets, only: [:create, :destory, :delete]
+  end
   resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
